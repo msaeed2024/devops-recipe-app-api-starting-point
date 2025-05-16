2b36c5184d7082530aa820e646bcdb988c88335f
msaeed2024 <muhamad.gamaleldin@gmail.com>
2025-05-16 17:35:47 +0300

    update infra config

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -10,7 +10,7 @@ variable "project" {
 
 variable "contact" {
   description = "Contact email for tagging resources"
-  default     = "mark@example.com"
+  default     = "muhamad.gamaleldin@gmail.com"
 }
 
 variable "db_username" {
